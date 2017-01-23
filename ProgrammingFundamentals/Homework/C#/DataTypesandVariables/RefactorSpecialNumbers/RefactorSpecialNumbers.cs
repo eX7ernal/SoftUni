979381ccc84cf3569b9c66c751fa4e9e8dd28719
eX7ernal <svetlin.nguen@gmail.com>
2017-01-23 12:59:47 +0200

    Data Types and Variables homework

---
+++ ProgrammingFundamentals/Homework/C#/DataTypesandVariables/RefactorSpecialNumbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorSpecialNumbers
+{
+    class RefactorSpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int num = 0;
+            bool special = false;
+            for (int i = 1; i <= n; i++)
+            {
+                num = i;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                special = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {special}");
+                sum = 0;
+            }
+        }
+    }
+}
---
+++ ProgrammingFundamentals/Homework/C#/DataTypesandVariables/SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialNumbers
+{
+    class SpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = 0;
+            bool check;
+            for (int i = 1; i <= n; i++)
+            {
+                int special = 0;
+                number = i;
+                while (number > 0)
+                {
+                    special += number % 10;
+                    number = number / 10;
+                }
+                if (special == 5 || special == 7 || special == 11)
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
+                Console.WriteLine("{0} ->{1}", i, check);
+            }
+        }
+    }
+}
